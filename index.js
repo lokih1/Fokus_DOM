df51c16c1a8d98d2680aaf0676054ec3c8eddf3a
lokih1 <lokihya@gmail.com>
2023-12-08 16:09:49 -0300

    Estudo DOM, forEach/play&pause

--- index.js
+++ index.js
@@ -5,19 +5,33 @@ const titulo = document.querySelector(".app__title");
 const botaoFoco = document.querySelector(".app__card-button--foco");
 const botaoCurto = document.querySelector(".app__card-button--curto");
 const botaoLongo = document.querySelector(".app__card-button--longo");
+const botaoAtivo = document.querySelectorAll(".app__card-button");
+const altMusica = document.querySelector("#alternar-musica")
+const sons = new Audio("/sons/luna-rise-part-one.mp3")
 
+sons.loop = true
 
+altMusica.addEventListener("change", function () {
+    if (sons.paused) {
+        sons.play()
+    } else {
+        sons.pause()
+    }
+})
 
 botaoFoco.addEventListener("click", function () {
   alterarContexto('foco')
+  botaoFoco.classList.add('active')
 })
 
 botaoCurto.addEventListener("click", function () {
     alterarContexto('descanso-curto')
+    botaoCurto.classList.add('active')
 })
 
 botaoLongo.addEventListener("click", function () {
     alterarContexto('descanso-longo')
+    botaoLongo.classList.add('active')
 })
 
 function alterarContexto (contexto) {
@@ -40,4 +54,10 @@ function alterarContexto (contexto) {
          default:
             break;
     }
-}
+    botaoAtivo.forEach(function(contexto) {
+        contexto.classList.remove('active')
+
+    }) 
+
+    }
+
